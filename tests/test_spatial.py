2de2726df616bda3a4993678bcefa5776ecfb4af
Guilherme Teixeira de Faria <gui.faria.96@hotmail.com>
2026-01-30 17:50:20 -0300

    test: adapta os testes para o `Span`

--- tests/test_spatial.py
+++ tests/test_spatial.py
@@ -1,97 +1,124 @@
-from anicrop.spatial import RelativeSpan
-from pytest import raises
+from anicrop.spatial import Span
+from pytest import raises, mark
 
 
-def test_RelativeSpan_com_start_igual_a_end():
-    expect = "Start '(0)' cannot be greater than end '(0)'."
+def test_Span_com_start_igual_a_end():
+    expect = "start must be < end (start=0, end=0)"
     with raises(ValueError) as excinfo:
-        RelativeSpan(0, 0)
+        Span(0, 0)
     result = str(excinfo.value)
     assert result == expect
 
 
-def test_RelativeSpan_com_start_maior_que_end():
-    expect = "Start '(1)' cannot be greater than end '(0)'."
+def test_Span_com_start_maior_que_end():
+    expect = "start must be < end (start=1, end=0)"
     with raises(ValueError) as excinfo:
-        RelativeSpan(1, 0)
+        Span(1, 0)
     result = str(excinfo.value)
     assert result == expect
 
 
-def test_RelativeSpan_com_start_maior_que_end_mas_negativo():
-    expect = "Start '(-1)' cannot be greater than end '(-10)'."
+def test_Span_com_start_negativo():
+    expect = 'start cannot be less than 0 (start=-1)'
     with raises(ValueError) as excinfo:
-        RelativeSpan(-1, -10)
+        Span(-1, -10)
     result = str(excinfo.value)
     assert result == expect
 
 
-def test_RelativeSpan_length_com_valores_positivos():
-    span = RelativeSpan(0, 1)
+def test_Span_com_end_negativo():
+    expect = "start must be < end (start=0, end=-1)"
+    with raises(ValueError) as excinfo:
+        Span(0, -1)
+    result = str(excinfo.value)
+    assert result == expect
+
+
+def test_Span_length_com_valores_positivos():
+    span = Span(0, 1)
     result = span.length
     assert result == 1
 
 
-def test_RelativeSpan_length_com_valores_negativos():
-    span = RelativeSpan(-11, -1)
-    result = span.length
-    assert result == 10
-
-
-def test_RelativeSpan_expand_com_valor_negativo():
-    expect = ("Margin for expand() must be non-negative. To contract "
-              "the span, use the shrink() method with a positive margin.")
+def test_Span_expand_com_valor_negativo():
+    expect = (
+        "Margin for expand() must be non-negative. To contract "
+        "the span, use the shrink() method with a positive margin."
+    )
     with raises(ValueError) as excinfo:
-        span = RelativeSpan(0, 2)
-        span.expand(-1)
+        Span(0, 10).expand(-1, Span(0, 20))
     result = str(excinfo.value)
     assert result == expect
 
 
-def test_RelativeSpan_expand_com_valor_zero():
-    span = RelativeSpan(0, 10).expand(0)
+def test_Span_expand_com_zero():
+    span = Span(0, 10).expand(0, Span(0, 20))
     result = span.length
     assert result == 10
 
 
-def test_RelativeSpan_expand_com_valor_positivo():
-    span = RelativeSpan(0, 10).expand(1)
+def test_Span_expand_com_start_zero():
+    span = Span(0, 10).expand(1, Span(0, 20))
     result = span.length
-    assert result == 12
+    assert result == 11
 
 
-def test_RelativeSpan_shrink_com_valor_negativo():
+def test_Span_expand_com_start_maior_que_zero():
+    span = Span(1, 10).expand(1, Span(0, 20))
+    result = span.length
+    assert result == 11
+
+
+def test_Span_expand_com_end_expandido_igual_ao_bound_end():
+    span = Span(1, 10).expand(1, Span(0, 11))
+    result = span.length
+    assert result == 11
+
+
+def test_Span_expand_com_end_expandido_maior_que_bound_end():
+    span = Span(1, 10).expand(2, Span(0, 11))
+    result = span.length
+    assert result == 11
+
+
+def test_Span_expand_com_start_expandido_menor_que_bound_start():
+    span = Span(6, 10).expand(2, Span(5, 11))
+    result = span.start
+    assert result == 5
+
+
+def test_Span_shrink_com_valor_negativo():
     expect = ("Margin for shrink() must be non-negative, but got -1. "
               "To expand the span, use the expand() method with a positive margin.")
     with raises(ValueError) as excinfo:
-        RelativeSpan(0, 2).shrink(-1)
+        Span(0, 2).shrink(-1)
     result = str(excinfo.value)
     assert result == expect
 
 
-def test_RelativeSpan_shrink_com_valor_zero():
-    span = RelativeSpan(0, 10).shrink(0)
+def test_Span_shrink_com_valor_zero():
+    span = Span(0, 10).shrink(0)
     result = span.length
     assert result == 10
 
 
-def test_RelativeSpan_shrink_com_valor_positivo():
-    span = RelativeSpan(0, 10).shrink(1)
+def test_Span_shrink_com_valor_positivo():
+    span = Span(0, 10).shrink(1)
     result = span.length
     assert result == 8
 
 
-def test_RelativeSpan_shrink_gerando_start_igual_a_end():
-    expect = "Start '(1)' cannot be greater than end '(1)'."
+def test_Span_shrink_gerando_start_igual_a_end():
+    expect = "start must be < end (start=1, end=1)"
     with raises(ValueError) as excinfo:
-        RelativeSpan(0, 2).shrink(1)
+        Span(0, 2).shrink(1)
     result = str(excinfo.value)
     assert result == expect
 
 
-def test_RelativeSpan_shrink_gerando_start_maior_que_end():
-    expect = "Start '(1)' cannot be greater than end '(0)'."
+def test_Span_shrink_gerando_start_maior_que_end():
+    expect = "start must be < end (start=1, end=0)"
     with raises(ValueError) as excinfo:
-        RelativeSpan(0, 1).shrink(1)
+        Span(0, 1).shrink(1)
     result = str(excinfo.value)
     assert result == expect
