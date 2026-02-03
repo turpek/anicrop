0753f0c79214c8222525812b9df8756b68ddb412
Guilherme Teixeira de Faria <gui.faria.96@hotmail.com>
2026-02-03 04:10:31 -0300

    feat: adiciona o `__getitem__` em `Image` para suporte de `Region`

--- src/anicrop/image.py
+++ src/anicrop/image.py
@@ -1,4 +1,6 @@
 from numpy import ndarray
+from anicrop.spatial import Region
+from typing import Any
 
 
 class Image:
@@ -15,6 +17,27 @@ class Image:
 
         self._data = image
         self._channels = image.shape[2] if image.ndim > 2 else 1
+
+    def __getitem__(self, key: Any):
+        if isinstance(key, Region):
+            region = (
+                slice(key.x.start, key.x.end),
+                slice(key.y.start, key.y.end),
+            )
+            return self._data[region]
+        if isinstance(key, tuple):
+            if isinstance(key[0], Region):
+                reg = key[0]
+                region = (
+                    slice(reg.x.start, reg.x.end),
+                    slice(reg.y.start, reg.y.end),
+                )
+                if any(isinstance(arg, Region) for arg in key[1:]):
+                    raise TypeError("Region must be the first and only spatial argument")
+                return self._data[region + key[1:]]
+        if any(isinstance(arg, Region) for arg in key):
+            raise TypeError("Region must be the first and only spatial argument")
+        return self._data[key]
 
     @property
     def shape(self) -> tuple[int, ...]:
