c7b874e093800c214b24f9fceb57a7a67724177a
Guilherme Teixeira de Faria <gui.faria.96@hotmail.com>
2026-02-06 03:21:11 -0300

    refactor: torna alguns atributos de `EditLayer` em property e setter

--- src/anicrop/layer.py
+++ src/anicrop/layer.py
@@ -44,10 +44,12 @@ class EditLayer:
             ValueError: If the provided image is fully transparent (has no content).
         """
         self.name = name
-        self.region = self._calculate_content_bbox(image)
+        self._region = self._calculate_content_bbox(image)
         bbox_image = image[self.region].copy()
         self.image = Image(bbox_image, image.format)
         self.opacity = opacity
+        self._rotate = 0.0
+        self._scale = 1.0
         self.blend_mode: BlendMode = blend_mode
 
     def _calculate_content_bbox(self, image: Image) -> Region:
@@ -79,3 +81,29 @@ class EditLayer:
         start_y, end_y = int(axis_y.min()), int(axis_y.max())
 
         return Region(Span(start_x, end_x + 1), Span(start_y, end_y + 1))
+
+    @property
+    def rotate(self) -> float:
+        return self._rotate
+
+    @rotate.setter
+    def rotate(self, rotate: float) -> None:
+        self._rotate = rotate
+
+    @property
+    def scale(self) -> float:
+        return self._scale
+
+    @scale.setter
+    def scale(self, scale: float) -> None:
+        self._scale = scale
+
+    @property
+    def region(self) -> Region:
+        return self._region
+
+    @region.setter
+    def region(self, region: Region) -> None:
+        if not isinstance(region, Region):
+            raise TypeError(f"Expected Region, got {type(region).__name__}")
+        self._region = region
