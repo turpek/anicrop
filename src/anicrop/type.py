7577f41fa6ea6a68ccddda413003727e956c6aff
Guilherme Teixeira de Faria <gui.faria.96@hotmail.com>
2026-02-07 23:39:30 -0300

    feat: criação de `TransDelta` e `Translation` para suportar deslocamento

--- src/anicrop/type.py
+++ src/anicrop/type.py
@@ -115,3 +115,71 @@ class Rotation:
 
     def __repr__(self):
         return f'{self.__class__.__name__}(Angle=({self._value}), Pivo=({self.pivo.x},{self.pivo.y}))'
+
+
+class TransDelta:
+    def __init__(self, value: int):
+        self._v = value
+
+    def __add__(self, other: int) -> None:
+        return self.__class__(self._v + int(other))
+
+    def __sub__(self, other: int) -> None:
+        return self.__class__(self._v - int(other))
+
+    def __int__(self):
+        return self._v
+
+    def __eq__(self, other: int) -> bool:
+        return self._v == int(other)
+
+    def __repr__(self) -> str:
+        return f'{self._v}'
+
+
+class Translation:
+
+    def __init__(self, x: int = 0, y: int = 0):
+        self._x = TransDelta(x)
+        self._y = TransDelta(y)
+
+    def _normalize(self, value: float | tuple[float, float, float]) -> tuple[float, float]:
+        if isinstance(value, int):
+            return (value, value)
+        elif isinstance(value, tuple):
+            return value
+        elif isinstance(value, Vector):
+            return value.x, value.y
+        raise NotImplementedError(f"Unsupported type: {type(value).__name__}")
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(x={self._x}, y={self._y})'
+
+    def __add__(self, value: int | tuple[int, int]) -> None:
+        dx, dy = self._normalize(value)
+        return Translation(self.x + dx, self.y + dy)
+
+    def __sub__(self, value: int | tuple[int, int]) -> None:
+        dx, dy = self._normalize(value)
+        return Translation(self.x - dx, self.y - dy)
+
+    def __eq__(self, other: Translation | Vector) -> bool:
+        if not isinstance(other, (self.__class__, Vector)):
+            raise NotImplementedError
+        return self.x == other.x and self.y == other.y
+
+    @property
+    def x(self) -> TransDelta:
+        return self._x
+
+    @x.setter
+    def x(self, value: int) -> None:
+        self._x = value
+
+    @property
+    def y(self) -> TransDelta:
+        return self._y
+
+    @y.setter
+    def y(self, value: int) -> None:
+        self._y = value
