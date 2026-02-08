9b37aaec927fe860b38525b198dd225b9f2d2f18
Guilherme Teixeira de Faria <gui.faria.96@hotmail.com>
2026-02-08 06:06:41 -0300

    test: suporte ao novo `__init__` do `RotationHandler`

--- tests/test_receiver.py
+++ tests/test_receiver.py
@@ -2,6 +2,7 @@ from anicrop.image import Image, ImageFormat
 from anicrop.layer import EditLayer, Layer
 from anicrop.receiver import RotationHandler
 from anicrop.spatial import Region
+from anicrop.proxy import ProxyLayer
 import operator
 # from pytest import raises
 import numpy as np
@@ -35,83 +36,106 @@ def img():
     return make_img()
 
 
+@pytest.fixture
+def layer():
+    return Layer(make_img())
+
+
+@pytest.fixture
+def canvas_mock(mocker):
+    mock = mocker.Mock()
+    mock.region = Region.from_size(500, 500)
+    return mock
+
+
+@pytest.fixture
+def proxy(mocker):
+    mock = mocker.Mock()
+    mock.region = Region.from_size(500, 500)
+    layer = Layer(make_img())
+    return ProxyLayer(layer, mock)
+
+
 @pytest.mark.parametrize("op,op_name", OPS, ids=[name for _, name in OPS])
 @pytest.mark.parametrize('value', VALUES, ids=('0', '-45', '+45'))
-def test_RotationHandler_layer_padrao_sem_EditLayers(img, value, op, op_name):
-    layer = Layer(img)
-    result = op(layer.rotate, value)
-    receiver = RotationHandler(layer, [], result)
+def test_RotationHandler_layer_padrao_sem_EditLayers(value, op, op_name, proxy):
+    result = op(proxy.rotate, value)
+    receiver = RotationHandler(proxy, result)
     receiver.rotate()
-    assert layer.rotate == result
+    assert proxy.rotate == result
+
+
+@pytest.mark.parametrize('value', [-90, 0, 90], ids=('-90', '0', '+90'))
+def test_RotationHandler_atribuicao(proxy, value):
+    proxy.rotate = value
+    assert proxy.rotate == value
 
 
 @pytest.mark.parametrize("op,op_name", OPS, ids=[name for _, name in OPS])
 @pytest.mark.parametrize('value', VALUES, ids=('0', '-45', '+45'))
-def test_RotationHandler_layer_rotacionado_sem_EditLayers(img, value, op, op_name):
-    layer = Layer(img)
-    layer.rotate += 45
-    result = op(layer.rotate, value)
-    receiver = RotationHandler(layer, [], result)
+def test_RotationHandler_layer_rotacionado_sem_EditLayers(value, op, op_name, proxy):
+    proxy._layer.rotate += 45
+    result = op(proxy.rotate, value)
+    receiver = RotationHandler(proxy, result)
     receiver.rotate()
-    assert layer.rotate == result
+    assert proxy.rotate == result
 
 
 @pytest.mark.parametrize("op,op_name", OPS, ids=[name for _, name in OPS])
 @pytest.mark.parametrize('value', VALUES, ids=('0', '-45', '+45'))
-def test_RotationHandler_layer_padrao_com_EditLayers_padrao(img, value, op, op_name):
+def test_RotationHandler_layer_padrao_com_EditLayers_padrao(img, value, op, op_name, proxy):
     edits = [EditLayer(img), EditLayer(img), EditLayer(img), EditLayer(img)]
-    layer = Layer(img)
-    result = op(layer.rotate, value)
-    receiver = RotationHandler(layer, edits, result)
+    proxy.add_edits(edits)
+    result = op(proxy.rotate, value)
+    receiver = RotationHandler(proxy, result)
     receiver.rotate()
 
-    assert layer.rotate == result
+    assert proxy.rotate == result
     for edit in edits:
         assert edit.rotate == result
 
 
 @pytest.mark.parametrize("op,op_name", OPS, ids=[name for _, name in OPS])
 @pytest.mark.parametrize('value', VALUES, ids=('0', '-45', '+45'))
-def test_RotationHandler_com_valores_variados(img, value, op, op_name):
+def test_RotationHandler_com_valores_variados(img, value, op, op_name, proxy):
     initial_rotations = [-30, -15, 15, 30]
     edits = [EditLayer(img) for _ in range(len(initial_rotations))]
     for edit, rot in zip(edits, initial_rotations):
         edit.rotate = rot
 
-    layer = Layer(img)
-    layer.rotate = 90
+    proxy._layer.rotate = 90
+    proxy.add_edits(edits)
 
-    result = op(layer.rotate, value)
-    receiver = RotationHandler(layer, edits, result)
+    result = op(proxy.rotate, value)
+    receiver = RotationHandler(proxy, result)
     receiver.rotate()
 
-    assert layer.rotate == result
+    assert proxy.rotate == result
     for edit, rot in zip(edits, initial_rotations):
         assert edit.rotate == op(rot, value)
 
 
 @pytest.mark.parametrize('value', VALUES_DIV)
-def test_RotationHandler_divisao_com_valores_variados(img, value):
+def test_RotationHandler_divisao_com_valores_variados(img, value, proxy):
     op = operator.truediv
     initial_rotations = [-90, -45, 45, 90]
     edits = [EditLayer(img) for _ in range(len(initial_rotations))]
     for edit, rot in zip(edits, initial_rotations):
         edit.rotate = rot
 
-    layer = Layer(img)
-    layer.rotate = 180
+    proxy._layer.rotate = 180
+    proxy.add_edits(edits)
 
-    result = op(layer.rotate, value)
-    receiver = RotationHandler(layer, edits, result)
+    result = op(proxy.rotate, value)
+    receiver = RotationHandler(proxy, result)
     receiver.rotate()
 
-    assert layer.rotate == result
+    assert proxy.rotate == result
     for edit, rot in zip(edits, initial_rotations):
         assert edit.rotate == op(rot, value)
 
 
-def test_RotationHandler_divisao_por_zero_levanta_erro(img):
-    layer = Layer(img)
-    layer.rotate = 90
+def test_RotationHandler_divisao_por_zero_levanta_erro(proxy):
+    proxy.rotate = 90
     with pytest.raises(ZeroDivisionError):
-        operator.truediv(layer.rotate, 0)
+        operator.truediv(proxy.rotate, 0)
