69c3c02d004098b271787e290179afb82b1fe2fa
Guilherme Teixeira de Faria <gui.faria.96@hotmail.com>
2026-02-19 00:44:18 -0300

    refactor: remove a classe `OperationFloat`

--- src/anicrop/type.py
+++ src/anicrop/type.py
@@ -1,6 +1,4 @@
 from __future__ import annotations
-from operator import add, sub, mul, truediv
-from typing import Optional
 from dataclasses import dataclass
 
 
@@ -16,54 +14,7 @@ class Vector:
         return iter((self.x, self.y))
 
 
-class OperationFloat(float):
-    """
-    Um float que lembra qual operação matemática o criou.
-    """
-    def __new__(cls, value, operation=None, origin_value=None):
-        return super().__new__(cls, value)
-
-    def __init__(self, value, operation=None, origin_value=None):
-        self.operation = operation
-        self.origin_value = origin_value
-
-    def __add__(self, other):
-        result = super().__add__(other)
-        return OperationFloat(result, operation=add, origin_value=float(other))
-
-    def __radd__(self, other):
-        result = super().__radd__(other)
-        return OperationFloat(result, operation=add, origin_value=float(other))
-
-    def __sub__(self, other):
-        result = super().__sub__(other)
-        return OperationFloat(result, operation=sub, origin_value=float(other))
-
-    def __rsub__(self, other):
-        result = super().__rsub__(other)
-        return OperationFloat(result, operation=sub, origin_value=float(other))
-
-    def __mul__(self, other):
-        result = super().__mul__(other)
-        return OperationFloat(result, operation=mul, origin_value=float(other))
-
-    def __rmul__(self, other):
-        result = super().__rmul__(other)
-        return OperationFloat(result, operation=mul, origin_value=float(other))
-
-    def __truediv__(self, other):
-        res = super().__truediv__(other)
-        return OperationFloat(res, operation=truediv, origin_value=float(other))
-
-    def __rtruediv__(self, other):
-        raise NotImplementedError
-        # res = super().__rtruediv__(other)
-        # return OperationFloat(res, operation=truediv, origin_value=float(other))
-
-    def __repr__(self):
-        return f"{float(self)}"
-
-
+@dataclass
 class Rotation:
     def __init__(self, value: float = 0.0, pivot: Vector = Vector(0.5, 0.5), operation=None, origin: Optional[float] = None):
         self._value = float(value)
