da5e45e00a1383eeef97e09bb2441d2d5caa66c2
Guilherme Teixeira de Faria <gui.faria.96@hotmail.com>
2026-02-19 19:45:28 -0300

    feat: cria os métodos `is_sealed`, `seal` e `can_merge`
    O método `seal` permite selar um comando, impedindo novas atualizações
    de valores, `is_sealed` verifica se o comando está selado já `can_merge`
    verifica se é possível fazer o merge do novo valor

--- src/anicrop/command.py
+++ src/anicrop/command.py
@@ -1,9 +1,17 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
+from anicrop.layer import Layer
 from typing import Any
 
 
 class Command(ABC):
+
+    def __init__(self, name: str, layer: Layer, value: Any):
+        self._sealed = False
+        self._layer = layer
+        self._new_state = value
+        self._name = name
+
     @abstractmethod
     def execute(self) -> None:
         ...
@@ -13,8 +21,17 @@ class Command(ABC):
         ...
 
     @abstractmethod
-    def update_value(self, value: Any) -> bool:
+    def update_value(self, value: Any) -> None:
         ...
 
-    def can_merge(self, command_cls: Command, layer: object) -> bool:
-        return isinstance(self, command_cls) and self._layer == layer
+    @property
+    def is_sealed(self) -> bool:
+        return self._sealed
+
+    def seal(self) -> None:
+        self._sealed = True
+
+    def can_merge(self, name: str, layer: object) -> bool:
+        if self.is_sealed:
+            return False
+        return self._name == name and self._layer == layer
