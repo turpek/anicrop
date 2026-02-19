3e5108b222896a6caedadae5243e9b1aad4bef76
Guilherme Teixeira de Faria <gui.faria.96@hotmail.com>
2026-02-19 19:50:19 -0300

    feat: cria o comando `SetAttributeCommand` para operar sobre os atributos do layer

--- src/anicrop/command.py
+++ src/anicrop/command.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from anicrop.layer import Layer
+from anicrop.spatial import Span, Region
+from anicrop.transform import calculate_new_bbox_from_layer
+from anicrop.type import RotationInput, ScaleInput, Transform
 from typing import Any
 
 
@@ -35,3 +38,27 @@ class Command(ABC):
         if self.is_sealed:
             return False
         return self._name == name and self._layer == layer
+
+
+class SetAttributeCommand(Command):
+
+    def __init__(
+            self,
+            name: str,
+            layer: Layer,
+            value: RotationInput | ScaleInput | Transform
+    ):
+        super().__init__(name, layer, value)
+        self._old_state = getattr(layer, name)
+
+    def execute(self) -> None:
+        setattr(self._layer, self._name, self._new_state)
+
+    def undo(self) -> None:
+        setattr(self._layer, self._name, self._old_state)
+
+    def update_value(self, value: Any) -> None:
+        self._new_state = value
+
+    def __repr__(self):
+        return f'{type(self).__name__}(name="{self._name}")'
